6f3d21812027c5dc0542fd443adcbd9131d3cd57
technewb <lauramneal3@gmail.com>
2019-04-25 16:15:53 -0400

    Removed extra spacing

--- categories/tests.py
+++ categories/tests.py
@@ -5,6 +5,5 @@ class CategoryModelTests(TestCase):
 
     def test_name_return_string(self):
         '''Return string of category name'''
-
         category_name = Category(name='Produce')
         self.assertIs(category_name.get_name(), 'Produce')
--- food/tests.py
+++ food/tests.py
@@ -4,5 +4,6 @@ from .models import Food
 class FoodModelTests(TestCase):
 
     def test_name_return_string(self):
+        
         food_name = Food(food_name='Beans')
         self.assertIs(food_name.get_name(), 'Beans')
