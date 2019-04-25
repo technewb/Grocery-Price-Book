7ee169648439cd773632b124f34b2826d2a59784
technewb <lauramneal3@gmail.com>
2019-04-25 16:18:56 -0400

    Added categories to installed_apps

--- Groceries/settings.py
+++ Groceries/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'stores.apps.StoresConfig',
     'food.apps.FoodConfig',
+    'categories.apps.CategoriesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
