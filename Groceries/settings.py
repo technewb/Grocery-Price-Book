f72500e3d3b86ba9b2ee6c429a38947847e683ee
technewb <lauramneal3@gmail.com>
2019-04-25 16:01:01 -0400

    Added food app to installed app and created migrations

--- Groceries/settings.py
+++ Groceries/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'stores.apps.StoresConfig',
+    'food.apps.FoodConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
