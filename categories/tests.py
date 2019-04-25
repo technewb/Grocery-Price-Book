c3305dab39cb6d345229dd2bee6970957c0efd3f
technewb <lauramneal3@gmail.com>
2019-04-25 16:15:22 -0400

    Added test for category names

--- categories/tests.py
+++ categories/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from .models import Category
 
-# Create your tests here.
+class CategoryModelTests(TestCase):
+
+    def test_name_return_string(self):
+        '''Return string of category name'''
+
+        category_name = Category(name='Produce')
+        self.assertIs(category_name.get_name(), 'Produce')
