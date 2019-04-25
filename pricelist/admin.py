9e454da10f725362c3698a3801400685868272cf
technewb <lauramneal3@gmail.com>
2019-04-25 18:38:09 -0400

    Updated admin view

--- pricelist/admin.py
+++ pricelist/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Category
+from .models import Category, Store, Food
 
 # Access categories in admin view
 admin.site.register(Category)
+admin.site.register(Store)
+admin.site.register(Food)
