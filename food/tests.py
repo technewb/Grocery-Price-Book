63b043e7fd00e06e175d5df61e260d50e265f2c5
technewb <lauramneal3@gmail.com>
2019-04-25 16:05:42 -0400

    changed assertEqual to assertIs

--- food/tests.py
+++ food/tests.py
@@ -5,4 +5,4 @@ class FoodModelTests(TestCase):
 
     def test_name_return_string(self):
         food_name = Food(food_name='Beans')
-        self.assertEqual(food_name.get_name(), 'Beans')
+        self.assertIs(food_name.get_name(), 'Beans')
