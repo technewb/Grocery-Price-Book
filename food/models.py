aa18488302b55dc76079cb378a1c4a5ebe9093f6
technewb <lauramneal3@gmail.com>
2019-04-25 15:50:53 -0400

    Add food model

--- food/models.py
+++ food/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Food(models.Model):
+    food_name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.food_name
