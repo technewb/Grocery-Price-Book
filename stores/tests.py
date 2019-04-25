5473e77220a90edcd489bbe33d215c10ed2c9835
technewb <lauramneal3@gmail.com>
2019-04-25 15:43:05 -0400

    Create food app, added tests for Store model

--- stores/tests.py
+++ stores/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Store
+
+class StoreModelTests(TestCase):
+
+    def test_store_name_return_string(self):
+        store_name = Store(store_name="Kroger")
+        self.assertIs(store_name.get_name(), "Kroger")
+    
+    def test_location_return_string(self):
+        store_name = Store(location="Dallas Hwy, Marietta, GA 30064")
+        self.assertIs(store_name.get_location(), "Dallas Hwy, Marietta, GA 30064")
