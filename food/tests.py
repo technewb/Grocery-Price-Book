c1d56884e52ae4238e0fa027986bb9e1861453d5
technewb <lauramneal3@gmail.com>
2019-04-25 16:16:26 -0400

    Added description for test in food

--- food/tests.py
+++ food/tests.py
@@ -4,6 +4,6 @@ from .models import Food
 class FoodModelTests(TestCase):
 
     def test_name_return_string(self):
-        
+        '''Return string of food name'''
         food_name = Food(food_name='Beans')
         self.assertIs(food_name.get_name(), 'Beans')
