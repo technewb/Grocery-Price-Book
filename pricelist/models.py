781ca4a9f545bfcae996962517d22fad62555968
technewb <lauramneal3@gmail.com>
2019-04-29 11:30:44 -0400

    +Unit descrip, formatting & removed custom props

--- pricelist/models.py
+++ pricelist/models.py
@@ -2,22 +2,22 @@ from django.db import models
 from django.utils import timezone
 from datetime import datetime
 
+
 class Category(models.Model):
     name = models.CharField(max_length=200)
 
     def __str__(self):
         return self.name
 
+
 class Food(models.Model):
     name = models.CharField(max_length=200)
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL, default='', null=True, blank=True)
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, default='', null=True, blank=True)
 
     def __str__(self):
         return f'{self.name} ({self.category})'
 
-    @property
-    def category_name(self):
-        return self.category.name
 
 class Store(models.Model):
     name = models.CharField(max_length=200)
@@ -26,44 +26,30 @@ class Store(models.Model):
     def __str__(self):
         return f'{self.name} - {self.location}'
 
+
 class Unit(models.Model):
+    '''Unit of measurement'''
     name = models.CharField(max_length=10)
 
     def __str__(self):
         return self.name
 
-    def get_unit(self):
-        return self.name
 
 class Price(models.Model):
     store = models.ForeignKey(Store, on_delete=models.CASCADE)
     food = models.ForeignKey(Food, on_delete=models.CASCADE)
-    brand = models.CharField(max_length=200)
+    brand = models.CharField(max_length=200, blank=True)
     price = models.DecimalField(max_digits=12, decimal_places=2)
     on_sale = models.BooleanField()
-    date = models.DateField(auto_now_add=True)
-    expiration_date = models.DateField(null=True)
+    date = models.DateField()
+    expiration_date = models.DateField(blank=True)
     # TODO: May need to figure out how to deal when using math
-    unit = models.ForeignKey(Unit, on_delete=models.SET_NULL, null=True, blank=True)
+    unit = models.ForeignKey(
+        Unit, on_delete=models.SET_NULL, null=True, blank=True)
     amount = models.DecimalField(max_digits=12, decimal_places=2)
 
     def __str__(self):
         return (f'{self.food} ${(self.price / self.amount):.2f} per {self.unit} - {self.store}')
-    
-    @property
-    def store_name(self):
-        '''Returns string of store name'''
-        return self.store.name
-    
-    @property
-    def food_name(self):
-        '''Returns string of food name'''
-        return self.food.name
-    
-    @property
-    def unit_name(self):
-        '''Returns string of unit name'''
-        return self.unit.name
 
     def is_sale_expired(self):
         '''Returns boolean if sale expired'''
