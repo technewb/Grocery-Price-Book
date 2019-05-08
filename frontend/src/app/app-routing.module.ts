20ec7a88e1c388b30377c711c82d47ab3c7bdb9d
technewb <lauramneal3@gmail.com>
2019-05-08 18:51:36 -0400

    Added descriptions

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,8 +3,13 @@ import { Routes, RouterModule } from "@angular/router";
 
 import { CategoryListComponent } from "./category-list/category-list.component";
 
+/**
+ * Routes to components
+ */
 const routes: Routes = [
+    // Redirects on load to categories
     { path: '', redirectTo: 'categories', pathMatch: 'full' },
+    // Other links and paths
     {
         path: 'categories',
         component: CategoryListComponent
@@ -12,7 +17,9 @@ const routes: Routes = [
 ];
 
 @NgModule({
+    // Import created module with routes directions
     imports: [RouterModule.forRoot(routes)],
+    // Export the module for use
     exports: [RouterModule]
 })
 export class AppRoutingModule { }
