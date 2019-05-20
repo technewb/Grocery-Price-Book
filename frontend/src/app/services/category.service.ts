354d296615244724ec0caca80a00c1d90e362054
technewb <lauramneal3@gmail.com>
2019-05-20 12:02:41 -0400

    Modified api calling, get cat by id and update cat

--- frontend/src/app/services/category.service.ts
+++ frontend/src/app/services/category.service.ts
@@ -1,5 +1,12 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Category } from '../category';
+import { catchError, map, tap } from "rxjs/operators";
+import { Observable, of } from 'rxjs';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json'})
+};
 
 @Injectable({
   providedIn: 'root'
@@ -8,11 +15,51 @@ import { HttpClient } from '@angular/common/http';
 // Grab and return Category API data from Django server
 export class CategoryService {
 
-  API_URL ='http://localhost:8000/api';
+  API_URL ='http://localhost:8000/api/categories';
 
   constructor(private httpClient: HttpClient) { }
 
-  getFirstPage() {
-    return this.httpClient.get(`${this.API_URL}/categories/`);
+  getCategories(): Observable<Category[]> {
+    return this.httpClient.get<Category[]>(this.API_URL)
+    .pipe(
+      tap(_ => console.log('fetched categories')),
+      catchError(this.handleError<Category[]>('getCategories', []))
+    );
   }
+
+  getCategory(id: number): Observable<Category> {
+    const url = `${this.API_URL}/${id}/`;
+    return this.httpClient.get<Category>(url).pipe(
+      tap(_ => console.log(`fetched category id=${id}`)),
+      catchError(this.handleError<Category>('getCategory id=${id}'))
+    );
+  }
+
+  /** PUT: update the category on the server */
+  updateCategory (category: Category, id: number): Observable<any> {
+    return this.httpClient.put(`${this.API_URL}/${id}/`, category, httpOptions).pipe(
+      tap(_ => console.log(`updated category id=${category.id}`)),
+      catchError(this.handleError<any>('updateCategory'))
+    );
+  }
+
+  /**
+ * Handle Http operation that failed.
+ * Let the app continue.
+ * @param operation - name of the operation that failed
+ * @param result - optional value to return as the observable result
+ */
+private handleError<T> (operation = 'operation', result?: T) {
+  return (error: any): Observable<T> => {
+ 
+    // TODO: send the error to remote logging infrastructure
+    console.error(error); // log to console instead
+ 
+    // TODO: better job of transforming error for user consumption
+    console.log(`${operation} failed: ${error.message}`);
+ 
+    // Let the app keep running by returning an empty result.
+    return of(result as T);
+  };
+}
 }
