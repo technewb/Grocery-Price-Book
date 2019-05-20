0a58f71eca08681ee015d4daf8cd269e80b12116
technewb <lauramneal3@gmail.com>
2019-05-20 12:05:23 -0400

    Remove extra error logger

--- frontend/src/app/services/category.service.ts
+++ frontend/src/app/services/category.service.ts
@@ -55,9 +55,6 @@ private handleError<T> (operation = 'operation', result?: T) {
     // TODO: send the error to remote logging infrastructure
     console.error(error); // log to console instead
  
-    // TODO: better job of transforming error for user consumption
-    console.log(`${operation} failed: ${error.message}`);
- 
     // Let the app keep running by returning an empty result.
     return of(result as T);
   };
