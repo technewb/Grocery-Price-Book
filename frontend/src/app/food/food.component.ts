47c2fc08182dfa2d8bfd3a85d9a80d83aa22332a
technewb <lauramneal3@gmail.com>
2019-05-26 19:36:24 -0400

    filter categories based on if food is in it

--- frontend/src/app/food/food.component.ts
+++ frontend/src/app/food/food.component.ts
@@ -9,9 +9,12 @@ import { Food, Category } from "../models";
 })
 export class FoodComponent implements OnInit {
 
-  // Hold data grabbed from Food API to objects
+  // Hold data grabbed from Food & Category API to objects
   private food: Food[];
   private categories: Category[];
+
+  // Holds data of categories that have food connected to it
+  private categoriesUsing: Category[];
 
   // Endpoints
   protected foodEndpoint = 'food';
@@ -23,22 +26,41 @@ export class FoodComponent implements OnInit {
 
   ngOnInit() {
     // Run get all food & categories
-    this.getAllFood();
-    this.getAllCategories();
+    this.getData();
   }
 
   /** Grabs data from api and puts it into food */
-  getAllFood() {
+  getData() {
+
+    // Hold array of category ids that have food connected to it
+    let usedCategories = [];
+
+    // Get food data via service
     this.genericService.getAll<Food>(this.foodEndpoint)
-      .subscribe(food => this.food = food);
-  }
+      .forEach(foodData => {
+        // Assign food all food data
+        this.food = foodData;
 
-  /** Grabs data from api and puts it into categories */
-  getAllCategories() {
+        // Loop thru each food item and push category id to usedCategories array if not there already
+        foodData.forEach(food => {
+          if(!usedCategories.includes(food.category_id)) {
+            usedCategories.push(food.category_id)
+          }
+        });
+    });
+    
+    // Get category data via service
     this.genericService.getAll<Category>(this.catEndPoint)
-      .subscribe(categories => this.categories = categories);
+      .subscribe(catData => {
+        // Assigned all category list data
+        this.categories = catData;
+
+        // Filter out any categories not in the usedCategories list and assign remaining to categoriesUsing
+        this.categoriesUsing = catData.filter(c =>  usedCategories.includes(c.id));
+    });
   }
 
+  // Add new food item to database
   add(name: string, category_id: number): void {
     name = name.trim();
     if (!name) { return; }
@@ -48,6 +70,7 @@ export class FoodComponent implements OnInit {
       });
   }
 
+  // Delete food item from database and filters it out of food variable on page
   delete(food: Food): void {
     this.food = this.food.filter(f => f !== food);
     this.genericService.delete(food, this.foodEndpoint).subscribe();
