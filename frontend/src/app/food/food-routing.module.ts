fc9ca5311164b4fba3fc863bd29e9194c871e22d
technewb <lauramneal3@gmail.com>
2019-05-29 12:49:44 -0400

    Updated food module routing

--- frontend/src/app/food/food-routing.module.ts
+++ frontend/src/app/food/food-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FoodListComponent } from './food-list/food-list.component';
+import { FoodDetailComponent } from './food-detail/food-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: FoodListComponent
+  },
+  {
+    path: ':id',
+    component: FoodDetailComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- frontend/src/app/food/food.module.ts
+++ frontend/src/app/food/food.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { FoodRoutingModule } from './food-routing.module';
 import { FoodListComponent } from './food-list/food-list.component';
 import { FoodDetailComponent } from './food-detail/food-detail.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [FoodListComponent, FoodDetailComponent],
   imports: [
     CommonModule,
-    FoodRoutingModule
+    FoodRoutingModule,
+    FormsModule
   ]
 })
 export class FoodModule { }
