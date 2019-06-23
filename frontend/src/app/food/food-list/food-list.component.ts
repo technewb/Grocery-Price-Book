c96556e74c4eff279d3804472c0afc3b656c3a25
technewb <lauramneal3@gmail.com>
2019-06-23 19:09:11 -0400

    Updated add and delete functions on food-list page
    * Closes Issue #9 - Fixed to dynamically update with new category & item
    addition to page that wasn't previously shown
    * Dynamically deletes empty category on food-list page
    * Removed getData to getFood and getCategories
    * Put getCategories at end of getFood to make sure food fully loads
    first due to categories load depends on food data first

--- frontend/src/app/food/food-list/food-list.component.ts
+++ frontend/src/app/food/food-list/food-list.component.ts
@@ -14,8 +14,11 @@ export class FoodListComponent implements OnInit {
   private food: Food[];
   private categories: Category[];
 
-  // Holds data of categories that have food connected to it
-  private categoriesUsing: Category[];
+  // Holds filtered data of categories using data from foodCategories
+  private filteredCategories: Category[];
+
+  // Hold array of category ids and count numbers of food within the categories
+  private foodCategories = [];
 
   // Endpoints
   protected foodEndpoint = 'food';
@@ -27,29 +30,35 @@ export class FoodListComponent implements OnInit {
 
   ngOnInit() {
     // Run get all food & categories
-    this.getData();
+    this.getFood();
   }
 
-  /** Grabs data from api */
-  getData() {
 
-    // Hold array of category ids that have food connected to it
-    let usedCategories = [];
-
+  getFood() {
     // Get food data via service
     this.genericService.getAll<Food>(this.foodEndpoint)
       .subscribe(foodData => {
         // Assign food all food data
         this.food = foodData;
 
-        // Loop thru each food item and push category id to usedCategories array if not there already
+        // Loop thru each food item and push category id to foodCategories with count array if not there already
+        // If data already exists, then increase count number
         foodData.forEach(food => {
-          if(!usedCategories.includes(food.category_id)) {
-            usedCategories.push(food.category_id)
+          if(!this.foodCategories.find(f => f.id === food.category_id)) {
+            this.foodCategories.push({'id': food.category_id, 'count': 1})
+          } else {
+            let index = this.foodCategories.findIndex(f => f.id === food.category_id);
+            this.foodCategories[index].count += 1;
           }
         });
+        console.log(this.foodCategories);
+        
+        // Run after getFood finishes in order to utilize data from food
+        this.getCategories()
     });
-    
+  }
+
+  getCategories() {
     // Get category data via service
     this.genericService.getAll<Category>(this.catEndPoint)
       .subscribe(catData => {
@@ -57,24 +66,69 @@ export class FoodListComponent implements OnInit {
         this.categories = catData;
 
         // Filter out any categories not in the usedCategories list and assign remaining to categoriesUsing
-        this.categoriesUsing = catData.filter(c =>  usedCategories.includes(c.id));
+        this.filteredCategories = catData.filter(c => this.foodCategories.find(f => f.id === c.id));
     });
   }
 
   // Add new food item to database
   add(name: string, category_id: number): void {
+    // Remove white space around name
     name = name.trim();
+
+    // Do nothing if no name is entered
     if (!name) { return; }
+
+    // If name entered, add as Food object to food api
     this.genericService.add({ name , category_id } as Food, this.foodEndpoint)
       .subscribe(f => {
-        this.food.push(f)
+        // Check if category id is already available on paid via foodCategories
+        let foodCatIndex = this.foodCategories.findIndex(c => f.category_id === c.id);
+
+        // If category id doesn't exist, add it to foodCategories array
+        if (foodCatIndex < 0) {
+          this.foodCategories.push({'id': f.category_id, 'count': 1});
+        } else {
+          // Otherwise, updated existing category count
+          this.foodCategories[foodCatIndex].count += 1;
+        }
+
+        // Update filteredCategories to updated array of foodCategories
+        this.filteredCategories = this.categories.filter(c => this.foodCategories.find(f => f.id === c.id));
+
+        // Push new food to list
+        this.food.push(f);
+
       });
   }
 
-  // Delete food item from database and filters it out of food variable on page
+  /**
+   * Deletes food item from api and removes from the page dynamically.
+   * Also removes category object visually if there is no existing food data within
+   * that category on the page
+   */
   delete(food: Food): void {
+    // Filters out deleted food
     this.food = this.food.filter(f => f !== food);
+
+    // Find foodCategories index based in category id
+    let foodCatIndex = this.foodCategories.findIndex(c => food.category_id === c.id);
+
+    // Subtract by one food category count on delete
+    this.foodCategories[foodCatIndex].count -= 1;
+
+    // Check if foodCategory count is less than 1
+    if(this.foodCategories[foodCatIndex].count < 1) {
+      // Find index of category object with less than count less than 1
+      let filteredCatIndex = this.filteredCategories.findIndex(c => this.foodCategories[foodCatIndex].id === c.id);
+
+      // Remove data related to category from foodCategory data array and filteredCategory object array
+      this.foodCategories.splice(foodCatIndex, 1);
+      this.filteredCategories.splice(filteredCatIndex, 1);
+    }
+
+    // Remove food from api
     this.genericService.delete(food, this.foodEndpoint).subscribe();
+    
   }
 
 }
